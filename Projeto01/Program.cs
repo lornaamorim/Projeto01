d58f51e22c2e6d6aa28666a453992064de8bb6bf
Usuario <Usuario@CPD03-M08>
2024-02-20 12:23:13 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo");
+
+            Console.WriteLine("Primeira aula de Lógica");
+
+            Console.WriteLine(2.5);
+
+            Console.WriteLine(2);
+
+            Console.WriteLine("2 + 2");
+
+            Console.WriteLine("O resultado é: " + (2 + 2));
+
+            Console.WriteLine(2 + 2 + " é o resultado");
+
+            Console.ReadKey();
+        }
+    }
+}
